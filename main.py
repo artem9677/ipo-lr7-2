28504f2a12351563ded7e222cff9b587c55fe99c
Artem <69763892+artem9677@users.noreply.github.com>
2024-11-16 09:18:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open("dump.json", 'r', encoding='utf-8') as file:
     data = json.load(file) 
     for skill in data:
         if skill.get("model") == "data.skill":
-            if skill["fields"].get("specialty") == number: 
+            if skill["fields"].get("code") == number: 
                 skill_code = skill["fields"].get("code")
                 skill_title = skill["fields"].get("title")
                 find = True
@@ -15,7 +15,7 @@ with open("dump.json", 'r', encoding='utf-8') as file:
                 for profession in data:
                     if profession.get("model") == "data.specialty":
                         specialty_code = profession["fields"].get("code")
-                        if specialty_code in skill_code:  
+                        if specialty_code in number:  
                             specialty_title = profession["fields"].get("title")
                             specialty_educational = profession["fields"].get("c_type")
                             
